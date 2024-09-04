e09d94cd1e0cb0f296f7954d1c384d39337a8adc
Regis Ricardo <regis.aricardo@gmail.com>
2024-09-03 23:38:33 -0300

    inserção do script criar maquina Ubuntu na azure

---
+++ criar_vm_azure.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Criar o Resource Group (caso ainda não tenha sido criado)
+az group create --name RG-lab-desafio-docker --location brazilsouth
+
+# Criar a VM Ubuntu 20.04 LTS com autenticação por senha e IP público padrão
+az vm create \
+  --name host-docker-ubuntu \
+  --resource-group RG-lab-desafio-docker \
+  --location brazilsouth \
+  --image Ubuntu2204 \
+  --admin-username regis \
+  --admin-password <inserir-senha> \
+  --authentication-type password \
+  --size Standard_B1s \
+  --public-ip-sku Standard \
+  --output json
+
